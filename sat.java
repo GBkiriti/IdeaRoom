c82d2eae3893f2fc2f5348f6cf6f326b3e87cb96
Kiriti Gunukuntla <krishh0808@gmail.com>
2018-09-24 19:41:15 +0530

    reset ideas.json

---
+++ sat.java
@@ -0,0 +1,31 @@
+class box{
+    double width;
+    double height;
+    double depth;
+    public void readValues()
+    {
+        Scanner scan=new Scanner(System.in);
+        System.out.println("enter the width height depth::");
+        width=scan.nextdouble();
+        height=scan.nextdouble();
+        depth=scan.nextDouble();
+
+    }
+    Public void displayvalues()
+    {
+        System.out.println("\nwidth ::"+width);
+        System.out.println("\nheight ::"+height);
+        System.out.println("\ndepth ::"+depth);
+    }
+
+    }
+    public class Recursion{
+        public string void main()
+    }
+
+
+
+
+
+
+}
